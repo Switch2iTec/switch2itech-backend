912dc621ba805cfae505e1421bf9296570beea06
Malik Zeeshan <mzeeshanmalik130@gmail.com>
2026-03-01 15:59:00 +0500

    Update CORS origin to production URL

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const app = express();
 app.use(
   cors({
     credentials: true,
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    origin: process.env.CLIENT_URL || "https://switch2itech.vercel.app",
   })
 );
 
